caff8bc97e6204549d28591864f413be605fb071
Kinoko <huiyuanai129@outlook.com>
2024-10-04 21:03:51 +0800

    first sql

---
+++ Datagrip/2.2.5.sql
@@ -0,0 +1,34 @@
+# view databases
+-- show Databases;
+-- this is a comment
+
+/*
+ multi lines comments
+ */
+
+-- use database
+use sakila;
+
+-- check the current used databases
+select database();
+
+-- view tables
+show tables;
+
+-- create new table
+create table student(
+    id int,
+    name varchar(20),
+    age int
+);
+
+-- delete table
+drop table student;
+
+-- charset utf8 no need
+create database test charset utf8;
+
+show databases;
+
+-- delete test database
+drop database test;
